8722a601e5135a5124e45a3453876d5b0fad7a8f
Paolo Cancedda <paolo.cancedda@gmail.com>
2017-05-15 23:25:29 +0200

    Fixes #1

--- n2temu/rom16loader.cpp
+++ n2temu/rom16loader.cpp
@@ -17,7 +17,7 @@ void ROM16Loader::load(const QString &fileName)
     if (inputFile.open(QIODevice::ReadOnly))
     {
         int currentSize = 0;
-        quint16 *pdata;
+        quint16 *pdata = NULL;
         QTextStream in(&inputFile);
         while (!in.atEnd())
         {
